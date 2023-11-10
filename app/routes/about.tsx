566074041ffb17b077cc93fd92507f8f9f71c93c
Michael Hespenheide <mahespenheide@gmail.com>
2023-11-10 01:09:50 -0500

    feat: About and contact

--- app/routes/about.tsx
+++
@@ -1,9 +0,0 @@
-import type { MetaFunction } from "@remix-run/node";
-
-export const meta: MetaFunction = () => {
-  return [{ title: "mhespenh.com | About" }];
-};
-
-export default function About() {
-  return <div>Coming soon...</div>;
-}
